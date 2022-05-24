b128d01f022006624028a371d1144e32e8e8ac77
UJ528491 <hyj0829@gmail.com>
2022-05-25 05:51:37 +0900

    set prisma

---
+++ users/users.typeDefs.ts
@@ -0,0 +1,22 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+    bio: String
+    avatar: String
+    followings: [User]
+    followers: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
+  }
+  type Query {
+    totalFollowing: Int!
+  }
+`;
